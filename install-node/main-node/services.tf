9652a0ca7c0d0d7ced7d594f3f9426ceb5b2b1f6
Nifemi Alpine Durin <k47sec@gmail.com>
2020-11-11 20:17:42 +0100

    update mysql  and sonar

--- install-node/main-node/services.tf
+++ install-node/main-node/services.tf
@@ -1,8 +1,42 @@
-  resource "helm_release" "data-service" {
-    name       = "octopusx-data"
-    chart      = "nodes/main-node/octopusx-data-chart"
+  resource "helm_release" "mysql-service" {
+    name       = "octopusx-mysql"
+    chart      = "nodes/main-node/octopusx-mysql-chart"
     namespace  = var.namespace
     #create_namespace =  true
 
   depends_on = [kubernetes_secret.docker_config,kubernetes_namespace.node]
   }
+
+  resource "helm_release" "rabbitmq-service" {
+  name       = "octopusx-rabbitmq"
+  chart      = "nodes/main-node/octopusx-rabbitmq-chart"
+  namespace  = var.namespace
+    #create_namespace =  true
+
+  depends_on = [kubernetes_secret.docker_config,kubernetes_namespace.node]
+  }
+
+
+
+  resource "helm_release" "sonarqube-service" {
+  name       = "octopusx-sonarqube"
+  chart      = "nodes/main-node/octopusx-sonarqube-chart"
+  namespace  = var.namespace
+    #create_namespace =  true
+
+  depends_on = [kubernetes_secret.docker_config,kubernetes_namespace.node]
+  }
+
+
+
+  resource "helm_release" "phpmyadmin-service" {
+  name       = "octopusx-phpmyadmin"
+  chart      = "nodes/main-node/octopusx-phpmyadmin-chart"
+  namespace  = var.namespace
+    #create_namespace =  true
+
+  depends_on = [kubernetes_secret.docker_config,kubernetes_namespace.node]
+  }
+
+
+
